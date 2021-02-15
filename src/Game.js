b303fc930c75d6d188acf061790128eca4ecfe5e
tayutaedomo <tayutaedomo@gmail.com>
2021-02-15 12:23:12 +0900

    add: no winner handling

--- src/Game.js
+++ src/Game.js
@@ -52,8 +52,12 @@ export default class Game extends React.Component {
     let status;
     let winnerLines = [];
     if (winner) {
-      status = 'Winner: ' + winner.symbol;
-      winnerLines = winner.lines;
+      if (winner.draw) {
+        status = 'No Winner';
+      } else {
+        status = 'Winner: ' + winner.symbol;
+        winnerLines = winner.lines;
+      }
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
@@ -92,6 +96,7 @@ function calculateWinner(squares) {
     [0, 4, 8],
     [2, 4, 6],
   ];
+
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
     if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
@@ -100,6 +105,12 @@ function calculateWinner(squares) {
         lines: lines[i].slice(),
       };
     }
+    else if (squares.includes(null) === false) {
+      return {
+        draw: true,
+      };
+    }
   }
+
   return null;
 }
